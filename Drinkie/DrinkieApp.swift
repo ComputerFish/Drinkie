ec0b7101783c5a3ecc6e80e0f16e8e4467b99ffa
ComputerFish <52689119+ComputerFish@users.noreply.github.com>
2024-07-14 13:44:54 -0700

    Initial Commit

---
+++ Drinkie/DrinkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkieApp.swift
+//  Drinkie
+//
+//  Created by Alvaro Avalos on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
