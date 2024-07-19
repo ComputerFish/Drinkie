181fb130500e9f1494221167b4de619837b6ad7f
ComputerFish <52689119+ComputerFish@users.noreply.github.com>
2024-07-19 00:46:18 -0700

    Added home view tabbing

--- Drinkie/ContentView.swift
+++ Drinkie/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeViewTab()
+                .tabItem {
+                    Label("Home", systemImage: "waterbottle")
+                }
         }
-        .padding()
     }
 }
 
